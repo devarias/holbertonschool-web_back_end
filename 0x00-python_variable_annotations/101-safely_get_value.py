79b62e31a352c22b3f863c0813b0458806fef430
David Arias <gedafu2005@gmail.com>
2022-02-02 09:26:58 -0500

    11. More involved type annotations

---
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""module doc"""
+
+from typing import Mapping, Any, Union, TypeVar
+
+T = TypeVar('T')
+
+
+def safely_get_value(dct: Mapping, key: Any,
+                     default: Union[None, T] = None) -> Union[T, Any]:
+    """function docs"""
+    if key in dct:
+        return dct[key]
+    else:
+        return default
