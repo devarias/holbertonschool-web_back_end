85f03489c5de166db20f10a008620bb447a24b49
David Arias <gedafu2005@gmail.com>
2022-02-02 08:18:38 -0500

    6. Complex types - mixed list

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -7,4 +7,4 @@ from typing import List, Union
 
 def sum_mixed_list(mxd_lst: List[Union[float, int]]) -> float:
     """function docs"""
-    return reduce(lambda a, b: a+b, mxd_lst)
+    return float(reduce(lambda a, b: a + b, mxd_lst))
