65413a114a227409fbdbced704e0d81eeff3b69e
David Arias <gedafu2005@gmail.com>
2022-02-02 11:14:08 -0500

    4. Tasks

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
 """module docs"""
-
 from typing import List
 
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def task_wait_n(n: int,  max_delay: int) -> List[float]:
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """function docs"""
     delay = []
     for _ in range(n):
