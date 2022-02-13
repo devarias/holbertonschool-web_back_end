044419cabcdadd76600e9899d6a046e319759ce0
David Arias <gedafu2005@gmail.com>
2022-02-13 16:35:22 -0500

    2. Hypermedia pagination

---
+++ 0x04-pagination/2-hypermedia_pagination.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+""" module docs """
+import csv
+from math import floor
+from typing import List
+
+
+def index_range(page, page_size):
+    """ method docs """
+    if page and page_size:
+        start = (page - 1) * page_size
+        return start,  start + page_size
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """ obtains the indexes and return corresponding pages """
+        assert type(page) == int and page > 0
+        assert type(page_size) == int and page_size > 0
+        start, end = index_range(page, page_size)
+        pages = []
+        if start >= len(self.dataset()):
+            return pages
+        pages = self.dataset()
+        return pages[start:end]
+
+    def get_hyper(self, page = 1, page_size = 10):
+        """ method docs """
+        assert type(page) == int and page > 0
+        assert type(page_size) == int and page_size > 0
+        total_pages = floor(len(self.dataset()) / page_size)
+        return {'page_size': len(self.get_page(page, page_size)),
+                'page': page,
+                'data': self.get_page(page, page_size),
+                'next_page': page + 1 if page + 1 < total_pages else None,
+                'prev_page': page - 1 if page > 1 else None,
+                'total_pages': total_pages
+                }
