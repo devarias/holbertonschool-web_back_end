dcb08d599ac5efb93dd5cf45a5b25e0d195b343b
David Arias <gedafu2005@gmail.com>
2022-02-13 15:27:14 -0500

    0. Basic dictionary

---
+++ 0x03-caching/0-basic_cache.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""  module docs """
+from base_caching import BaseCaching
+
+class BasicCache(BaseCaching):
+    """class docs"""
+    def put(self, key, item):
+        """ method docs """
+        if key and item:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """ method docs """
+        if key is None or self.cache_data.get(key) is None:
+            return None
+        return self.cache_data.get(key)
