570fb487b60d5b73d341c203864ea9b3415263b8
David Arias <gedafu2005@gmail.com>
2022-02-13 15:32:25 -0500

    1. FIFO caching

---
+++ 0x03-caching/1-fifo_cache.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+""" module docs """
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """ class docs """
+
+    def __init__(self):
+        super().__init__()
+        self.data = {}
+        self.next_in, self.next_out = 0, 0
+
+    def _pop(self):
+        """ method docs """
+        self.next_out += 1
+        key = self.data[self.next_out]
+        del self.data[self.next_out], self.cache_data[key]
+
+    def _push(self, key, item):
+        """ method docs """
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS - 1:
+            print("DISCARD: {}".format(self.data[self.next_out + 1]))
+            self._pop()
+        self.cache_data[key] = item
+        self.next_in += 1
+        self.data[self.next_in] = key
+
+    def put(self, key, item):
+        """ method docs """
+        if key and item:
+            if key in self.cache_data:
+                self.cache_data[key] = item
+            else:
+                self._push(key, item)
+
+    def get(self, key):
+        """ method docs """
+        if key in self.cache_data:
+            value = self.cache_data[key]
+            return value
+        if key is None or self.cache_data.get(key) is None:
+            return None
