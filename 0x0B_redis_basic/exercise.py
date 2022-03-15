c3adbb67b3a30279dd8c79089f02fbe6befc2318
David Arias <gedafu2005@gmail.com>
2022-03-14 23:37:20 -0500

    2. Incrementing values 3. Storing lists 4. Retrieving lists

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -2,9 +2,63 @@
 """ module docs """
 
 
-from typing import Callable, Optional, Union
-from uuid import uuid4
+from functools import wraps
 import redis
+from typing import Union, Optional, Callable
+from uuid import uuid4
+
+
+def count_calls(method: Callable) -> Callable:
+    """ method doc """
+
+    key = method.__qualname__
+
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """ method doc """
+        self._redis.incr(key)
+        return method(self, *args, **kwargs)
+    return wrapper
+
+
+def call_history(method: Callable) -> Callable:
+    """ method doc """
+
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """ method doc """
+        input = str(args)
+        self._redis.rpush(method.__qualname__ + ":inputs", input)
+        output = str(method(self, *args, **kwargs))
+        self._redis.rpush(method.__qualname__ + ":outputs", output)
+        return output
+    return wrapper
+
+
+def replay(fn: Callable):
+    """ method doc """
+    r = redis.Redis()
+    f_name = fn.__qualname__
+    n_calls = r.get(f_name)
+    try:
+        n_calls = n_calls.decode('utf-8')
+    except Exception:
+        n_calls = 0
+    print(f'{f_name} was called {n_calls} times:')
+
+    ins = r.lrange(f_name + ":inputs", 0, -1)
+    outs = r.lrange(f_name + ":outputs", 0, -1)
+
+    for input, output in zip(ins, outs):
+        try:
+            input = input.decode('utf-8')
+        except Exception:
+            input = ""
+        try:
+            output = output.decode('utf-8')
+        except Exception:
+            output = ""
+        print(f'{f_name}(*{input}) -> {output}')
 
 
 class Cache:
@@ -16,6 +70,8 @@ class Cache:
         """ constructor docs """
         self._redis.flushdb()
 
+    @call_history
+    @count_calls
     def store(self, data: Union[bytes, float, int, str]) -> str:
         """ method docs """
         random_key = str(uuid4())
