2e355058126d0821da65ff00a39d40e1041ae70d
David Arias <gedafu2005@gmail.com>
2022-03-17 22:37:53 -0500

    12. Average weighted score

--- 0x0C-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x0C-MySQL_Advanced/100-average_weighted_score.sql
@@ -1,28 +1,15 @@
 -- sql script docs
 DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUser;
-
 DELIMITER / /
-CREATE PROCEDURE ComputeAverageWeightedScoreForUser(user_id INT)
-BEGIN DECLARE w_avg_score FLOAT;
-
-SET
-	w_avg_score = (
-		SELECT
-			SUM(score * weight) / SUM(weight)
-		FROM
-			users AS U
-			JOIN corrections as C ON U.id = C.user_id
-			JOIN projects AS P ON C.project_id = P.id
-		WHERE
-			U.id = user_id
-	);
-
-UPDATE
-	users
-SET
-	average_score = w_avg_score
-WHERE
-	id = user_id;
-
+CREATE PROCEDURE ComputeAverageWeightedScoreForUser(
+  user_id INT)
+BEGIN
+  DECLARE w_avg_score FLOAT;
+  SET w_avg_score = (SELECT SUM(score * weight) / SUM(weight)
+    FROM users AS U
+    JOIN corrections as C ON U.id=C.user_id
+    JOIN projects AS P ON C.project_id=P.id
+    WHERE U.id=user_id);
+  UPDATE users SET average_score = w_avg_score WHERE id=user_id;
 END;
 / /
