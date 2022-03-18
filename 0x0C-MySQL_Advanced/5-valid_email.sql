8984298cd9413c2e057d7a5f6e2d2fa82b30adee
David Arias <gedafu2005@gmail.com>
2022-03-17 22:25:18 -0500

    5. Email validation to sent

--- 0x0C-MySQL_Advanced/5-valid_email.sql
+++ 0x0C-MySQL_Advanced/5-valid_email.sql
@@ -1,5 +1,5 @@
 -- sql script docs
-delimiter / / CREATE TRIGGER email_bool BEFORE
+delimiter | CREATE TRIGGER email_bool BEFORE
 UPDATE
 	ON users FOR EACH ROW BEGIN IF NEW.email <> OLD.email THEN
 SET
@@ -9,4 +9,4 @@ END IF;
 
 END;
 
-/ /
+|
