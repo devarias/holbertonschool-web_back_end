f2100cc033dba9c6c69791dbac65f798f7f64d8d
David Arias <gedafu2005@gmail.com>
2022-03-24 01:31:47 -0500

    14. Iterate through object

--- 0x0E-ES6_basic/101-iterateThroughObject.js
+++ 0x0E-ES6_basic/101-iterateThroughObject.js
@@ -1,3 +1,11 @@
 export default function iterateThroughObject(reportWithIterator) {
-  return reportWithIterator.join(" | ");
+  //   return reportWithIterator.join(' | ');
+  let names;
+  for (const item of reportWithIterator) {
+    names.concat(item);
+    if (!item.done) {
+      names.concat(" | ");
+    }
+  }
+  return names;
 }
